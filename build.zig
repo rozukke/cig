0d01fec1c15b704b6abe0020ff57ab33a1fe755f
Artemis Rosman <73006620+rozukke@users.noreply.github.com>
2025-02-24 17:46:53 +1100

    fix: remove unnecessary build sections

--- build.zig
+++ build.zig
@@ -4,15 +4,6 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "ciglib",
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    b.installArtifact(lib);
-
     const exe_module = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
